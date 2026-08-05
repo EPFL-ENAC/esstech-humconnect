b900ac5ffd9c7622fb81264ca4caf25cbc943170
Son Pham-Ba <son.phamba@epfl.ch>
2026-08-05 10:24:42 +0200

    chore: change keycloak reaml to external

--- frontend/public/env.js
+++ frontend/public/env.js
@@ -2,7 +2,7 @@ window.env = {
     API_URL: 'http://localhost:8000',
     API_PATH: '',
     KEYCLOAK_URL: 'https://enac-it-sso2.epfl.ch',
-    KEYCLOAK_REALM: 'EPFL',
+    KEYCLOAK_REALM: 'external',
     AUTH_CLIENT_ID: 'humconnect-dev',
     SENTRY_ENVIRONMENT: 'local',
     SENTRY_RATE: '0.00',
